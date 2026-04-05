34ed17963d10b540e6df695f815c092cb9a56b79
Sominath1212 <girnaresominath@gmail.com>
2026-04-06 00:28:34 +0530

    repositories and interfaces added

---
+++ Zest.Domain/Interfaces/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+using Zest.Domain.Entities;
+
+namespace Zest.Domain.Interfaces
+{
+    public interface IAuthRepository
+    {
+        Task<User?> GetByEmailAsync(string email);
+        Task<bool> IsEmailExistsAsync(string email);
+        Task RegisterAsync(User user);
+    }
+}
